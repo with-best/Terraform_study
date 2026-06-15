3ce37fe674a1a19d21892b875669bfd23fd244d5
SangWon Park <psw7528@gmail.com>
2026-06-15 21:59:45 +0900

    add first terraform resource

---
+++ 03.start/main.tf
@@ -0,0 +1,8 @@
+resource "local_file" "abc" {
+  content  = "abc!"
+  filename = "${path.module}/abc.txt"
+}
+resource "local_file" "def" {
+  content  = "def!"
+  filename = "${path.module}/def.txt"
+}
